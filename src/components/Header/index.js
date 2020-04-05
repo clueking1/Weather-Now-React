1171c9aab541768f9d063f3433fa14bba9b206a8
clueking1 <wcoan300@gmail.com>
2020-04-05 15:32:39 -0400

    added Weather Now header unstyled

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './style.css'
+
+function Header() {
+
+
+    return (
+    <header className='head'>
+        <h1 className='headTitle'>Weather Now</h1>
+    </header>
+    )
+}
+
+export default Header
