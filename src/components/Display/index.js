19c08878da6262222ec1dbdd92ee8b705c246ae8
clueking1 <wcoan300@gmail.com>
2020-04-05 16:55:37 -0400

    all weather data from search is going to the display page

---
+++ src/components/Display/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Display(props) {
+
+    return (
+        <div>
+            <h3>Display</h3>
+        </div>
+    )
+}
+
+export default Display
--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,9 +1,20 @@
 import React, { useState } from 'react'
 import API from '../../utils/weatherAPI'
+import Display from '../Display'
 
 function Search() {
     const [city, setCity] = useState('')
     const [usstate, setUsState] = useState('')
+    const [displayInfo, setDisplayInfo] = useState({
+      temp : '',
+      tempFeels : '',
+      tempMin : '',
+      tempMax : '',
+      humid : '',
+      wind : '',
+      icon : '',
+      descrip : ''
+    })
 
 
     function handleSubmit(e) {
@@ -14,7 +25,17 @@ function Search() {
     function searchAPI() {
         API.searchTerms(`${city},${usstate}`)
             .then(res => {
-                console.log(res.data)
+
+                setDisplayInfo({
+                    temp : res.data.main.temp,
+                    tempFeels : res.data.main.feels_like,
+                    tempMin : res.data.main.temp_min,
+                    tempMax : res.data.main.temp_max,
+                    humid : res.data.main.humidity,
+                    wind : res.data.wind.speed,
+                    icon : res.data.weather[0].icon,
+                    descrip : res.data.weather[0].description
+                  })
             })
             
     }
@@ -22,33 +43,38 @@ function Search() {
 
 
     return (
-        <div className='displayWrapper'>
-            <div className='displayFormWrapper'>
-                <form onSubmit={handleSubmit}>
-                    <div className='displayCity'>
-                        <input 
-                            className='form-control'
-                            type='text'
-                            placeholder='City'
-                            name="city"
-                            onChange={e => setCity(e.target.value)}
-                        />
-                    </div>
-                    <div className='displayState'>
-                        <input 
-                            className='form-control'
-                            type='text'
-                            placeholder='State'
-                            name="state"
-                            onChange={e => setUsState(e.target.value)}
-                        />
-                    </div>
-                    <button className="subBtn" type="submit">
-                        Get Weather!
-                    </button>
-                </form>
+        <div>
+            <div className='displayWrapper'>
+                <div className='displayFormWrapper'>
+                    <form onSubmit={handleSubmit}>
+                        <div className='displayCity'>
+                            <input 
+                                className='form-control'
+                                type='text'
+                                placeholder='City'
+                                name="city"
+                                onChange={e => setCity(e.target.value)}
+                            />
+                        </div>
+                        <div className='displayState'>
+                            <input 
+                                className='form-control'
+                                type='text'
+                                placeholder='State'
+                                name="state"
+                                onChange={e => setUsState(e.target.value)}
+                            />
+                        </div>
+                        <button className="subBtn" type="submit">
+                            Get Weather!
+                        </button>
+                    </form>
+                </div>
             </div>
-        </div>
+            <div>
+                <Display info = {displayInfo}/>
+            </div>
+        </div>    
     )
 }
 
