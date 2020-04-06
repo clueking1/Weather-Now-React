f32b1ca4c7fd75958beb4049f6314f29ef402538
clueking1 <wcoan300@gmail.com>
2020-04-05 20:06:03 -0400

    all divs attaining to temp in place

--- src/components/Display/index.js
+++ src/components/Display/index.js
@@ -12,38 +12,38 @@ function Display(props) {
     return (
         <div className="displayWrapper">
             <div className='citStateDiv'>
-               <h3 className='stateHead'>{info.stateCity}</h3>
+               <h3 className='stateHead'>Cleveland, Ohio</h3>
            </div>
            <div className='dateDiv'>
-               <h3 className='dateText'>{month}/{day}</h3>
+               <h3 className='dateText'>4/5</h3>
            </div>
            <div className='iconDiv'>
                <img src= {iconurl} alt= 'icon'/>
            </div>
  
            <div className='tempDiv'>
-               <p className='tempP'>{info.temp}<span>&#8457;</span></p>
+               <p className='tempP'>60<span>&#8457;</span></p>
            </div>
            <div className='feelsTempDiv'>
-               <p className='feelsTempP'><span className='feelText'>Feels like </span>{info.tempFeels}<span>&#8457;</span></p>
+               <p className='feelsTempP'><span className='feelText'><i>Feels like </i> </span>70<span>&#8457;</span></p>
            </div>
            <div className='maxTempDiv'>
-               <p className='maxTempP'><span className='maxText'>Max </span>{info.tempMax}<span>&#8457;</span></p>
+               <p className='maxTempP'><span className='maxText'><i>Max: </i> </span> 80<span>&#8457;</span></p>
            </div>
            <div className='minTempDiv'>
-               <p className='minTempP'><span className='feelText'>Min </span>{info.tempMin}<span>&#8457;</span></p>
+               <p className='minTempP'><span className='feelText'><i>Min: </i></span> 50<span>&#8457;</span></p>
            </div>
            <div className='humidHeadDiv'>
                <p className='humidHeadP'>Humidity</p>
            </div>
            <div className='humidDiv'>
-               <p className='humidP'>{info.humid}%</p>
+               <p className='humidP'>60%</p>
            </div>
            <div className='windHeadDiv'>
                <p className='windHeadP'>Wind</p>
            </div>
            <div className='windDiv'>
-               <p className='windP'>{info.wind}mph</p>
+               <p className='windP'>5mph</p>
            </div>
 
         </div>
