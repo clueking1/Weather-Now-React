0aeb58c38f1d34ff2b2cb8a16bb91bfd00185386
clueking1 <wcoan300@gmail.com>
2020-04-06 13:26:00 -0400

    new build

---
+++ build/precache-manifest.3f20dfb97a8cf9a1c952598f71625112.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1594ba493a653ad1b52c435cc42fa57a",
+    "url": "/weather-now-react/index.html"
+  },
+  {
+    "revision": "9d3826dae8d4986391ee",
+    "url": "/weather-now-react/static/css/main.7731f2bd.chunk.css"
+  },
+  {
+    "revision": "22f7d46fecdddc432d9a",
+    "url": "/weather-now-react/static/js/2.8f2f837e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-now-react/static/js/2.8f2f837e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d3826dae8d4986391ee",
+    "url": "/weather-now-react/static/js/main.6f4dba3d.chunk.js"
+  },
+  {
+    "revision": "5c1618512e41f6311577",
+    "url": "/weather-now-react/static/js/runtime-main.b80f7efa.js"
+  },
+  {
+    "revision": "2b4cf6769d9fb62b9c2a0849f1b24c03",
+    "url": "/weather-now-react/static/media/Cloudy.2b4cf676.png"
+  }
+]);
